65cd4808dfb35962d966fa3dedd7047110455d2c
AnimatronicEmulator <leslie.durkey@gmail.com>
2024-09-30 18:27:02 -0600

    django-heroku specific prod/deployment junk

--- djangoregex/settings.py
+++ djangoregex/settings.py
@@ -31,18 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "regex_app.apps.RegexAppConfig",
-    "re_app.apps.ReAppConfig",
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "regex_app.apps.RegexAppConfig",
+    "re_app.apps.ReAppConfig",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
